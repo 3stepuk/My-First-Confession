a51c4e66e197926a0024ac33daee050d66304c58
Mark Harris <mark@3step.uk>
2026-08-28 15:38:40 +0100

    Set Pages base path (/My-First-Confession/)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/My-First-Confession/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
